4df6d808354257638a3e3d8a88420bbace26630e
Xenny76 <dredford@student.neumont.edu>
2025-08-27 15:21:52 -0600

    Finished endings

--- telltaleneumont/components/GoodRoute/GoodEnding.tsx
+++ telltaleneumont/components/GoodRoute/GoodEnding.tsx
@@ -6,7 +6,7 @@ export default function GoodEnding() {
       className="relative font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[100dvh] p-8 pb-20 gap-16 sm:p-20 bg-no-repeat bg-cover bg-center"
       style={{
         backgroundImage:
-          "url('')",
+          "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F1393720.jpg&f=1&nofb=1&ipt=a087cfadc5b2f0292067ec01aecdeb98c7c3bf582ab426fbe074ae7c28e69d82')",
       }}
     >
       <div className="absolute inset-0 bg-black/50 pointer-events-none" aria-hidden="true" />
--- telltaleneumont/components/NeutralRoute/NeutralEnding.tsx
+++ telltaleneumont/components/NeutralRoute/NeutralEnding.tsx
@@ -6,7 +6,7 @@ export default function Neutralending() {
       className="relative font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[100dvh] p-8 pb-20 gap-16 sm:p-20 bg-no-repeat bg-cover bg-center"
       style={{
         backgroundImage:
-          "url('')",
+          "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fget.pxhere.com%2Fphoto%2Foffice-job-white-collar-worker-design-collaboration-interior-design-event-technology-learning-employment-team-conversation-meeting-software-engineering-company-computer-business-building-1555089.jpg&f=1&nofb=1&ipt=fbe472156f95cae702115d416f1e2acfbf1d0af28128ed8a7c5db5c988d772d9')",
       }}
     >
       <div className="absolute inset-0 bg-black/50 pointer-events-none" aria-hidden="true" />
